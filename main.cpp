71096f00b3aea2bb4341e497a6859c2e833d4b24
= <ivank@interlink-ua.com>
2011-02-03 11:37:18 +0200

    show process

--- main.cpp
+++ main.cpp
@@ -14,10 +14,6 @@ int main(int argc, char *argv[])
 
     systemWin32 processesInWin;
 
-    int ret = QMessageBox::information(0, "Question", "Do you want see all list of process?", QMessageBox::Ok | QMessageBox::Cancel);
-
-    if (ret == QMessageBox::Ok)
-    {
         QStringList list(processesInWin.getAllProcessList());
         QString strings;
 
@@ -28,7 +24,6 @@ int main(int argc, char *argv[])
 
         label->setText(strings);
         label->show();
-    }
 
 
     return app.exec();
